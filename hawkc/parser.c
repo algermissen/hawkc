7e8b29b748db2cb9a321d8c043d67b02be39f58a
Jan Algermissen <algermissen@acm.org>
2013-07-23 06:30:36 +0200

    Cleaned up and documented parser.c

--- hawkc/parser.c
+++ hawkc/parser.c
@@ -5,14 +5,26 @@
 #include "hawkc.h"
 #include "common.h"
 
+#define DQUOTE '"';
+#define BACKSLASH '\\';
+
+#define IS_TOKEN(c) ( \
+	   ( (c) >= '0' && (c) <= '9') \
+	|| ( (c) >= 'A' && (c) <= 'Z') \
+	|| ( (c) >= '^' && (c) <= 'z') \
+	|| ( (c) >= '#' && (c) <= '\'') \
+	|| ( (c) == '!') || ((c) == '*') || ((c) == '+') || ((c) == '-') || ((c) == '.') )
+
+#define IS_SPACE(c) ( ((c) == ' ') || ((c) == '\t') )
+
 static void consume_ows(HawkcContext ctx, char *s, size_t len, size_t *n);
 static HawkcError parse_token(HawkcContext ctx, char *s, size_t len, HawkcString *ptoken, size_t *n);
 static HawkcError parse_quoted_text(HawkcContext ctx, char *s, size_t len, HawkcString *ptoken, size_t *n);
 static HawkcError parse_time(HawkcContext ctx, HawkcString ts, time_t *tp);
 
-#define DQUOTE '"';
-#define BACKSLASH '\\';
-
+/*
+ * Scheme callback for parsing authorization header.
+ */
 static HawkcError scheme_handler(HawkcContext ctx,HawkcString scheme,void *data) {
 	if((scheme.len != 4) || strncmp(scheme.data,"Hawk",4) != 0) {
 		return hawkc_set_error(ctx,
@@ -20,6 +32,10 @@ static HawkcError scheme_handler(HawkcContext ctx,HawkcString scheme,void *data)
 	}
 	return HAWKC_OK;
 }
+
+/*
+ * Parameter callback for parsing authorization header.
+ */
 static HawkcError param_handler(HawkcContext ctx,HawkcString key, HawkcString value,void *data) {
 
 	AuthorizationHeader h = (AuthorizationHeader)data;
@@ -72,40 +88,24 @@ HawkcError parse_time(HawkcContext ctx, HawkcString ts, time_t *tp) {
 	return HAWKC_OK;
 }
 
-
 HawkcError hawkc_parse_authorization_header(HawkcContext ctx, char *value, size_t len) {
-	HawkcError e;
-
-	/* FIXME: check and clear existing buffer and members */
-	/* FIXME: use better approach...
-	assert(ctx->header_in.buf == NULL);
-	assert(ctx->header_in.buf_len == 0);
-	assert(ctx->header_in.buf_pos == NULL);
-
-	ctx->header_in.buf = hawkc_alloc(ctx,len);
-	* FIXME check null *
-	ctx->header_in.buf_len = len;
-	ctx->header_in.buf_pos = ctx->header_in.buf;
-	*/
-
-
-	if( (e = hawkc_parse_auth_header(ctx,value,len,scheme_handler, param_handler,&(ctx->header_in))) != HAWKC_OK) {
-		return e;
-	}
-	return HAWKC_OK;
-
+	return hawkc_parse_auth_header(ctx,value,len,scheme_handler, param_handler,&(ctx->header_in));
 }
 
 
-
+/*
+ * See common.h for docs.
+ */
 HawkcError hawkc_parse_auth_header(HawkcContext ctx, char *value, size_t len, HawkcSchemeHandler scheme_handler, HawkcParamHandler param_handler, void *data) {
-
 	HawkcError e;
 	char *p = value;
 	size_t remain = len;
 	size_t n;
 	HawkcString scheme;
 
+	/*
+	 * Parse scheme part.
+	 */
 	if( (e = parse_token(ctx,p,len,&scheme,&n)) != HAWKC_OK) {
 		return e;
 	}
@@ -124,6 +124,11 @@ HawkcError hawkc_parse_auth_header(HawkcContext ctx, char *value, size_t len, Ha
 		return HAWKC_OK;
 	}
 
+	/*
+	 * While we have more to parse, consume key/value pairs.
+	 * token68 syntax is not supported. We always expect
+	 * key=value or key="value".
+	 */
 	while(remain > 0) {
 		HawkcString key, value;
 		if( (e = parse_token(ctx,p,remain,&key,&n)) != HAWKC_OK) {
@@ -131,21 +136,30 @@ HawkcError hawkc_parse_auth_header(HawkcContext ctx, char *value, size_t len, Ha
 		}
 		p += n;
 		remain -= n;
+
+		/* There can be optional WS after key-token */
 		consume_ows(ctx,p,remain,&n);
 		p += n;
 		remain -= n;
+
+		/* There must be a = now */
 		if(*p != '=') {
-			return hawkc_set_error(ctx,
-								HAWKC_PARSE_ERROR, "Missing '=' for parameter value");
+			return hawkc_set_error(ctx, HAWKC_PARSE_ERROR, "Missing '=' for parameter value");
 		}
 		/* consume '=' */
 		p++;
 		remain--;
 
+
+		/* There can be optional WS between = and value */
 		consume_ows(ctx,p,remain,&n);
 		p += n;
 		remain -= n;
 
+		/*
+		 *  Use first char of value to determine whether to consume
+		 * quoted string or token.
+		 */
 		if(*p == '"') {
 			if( (e = parse_quoted_text(ctx,p,remain,&value,&n)) != HAWKC_OK) {
 					return e;
@@ -160,15 +174,17 @@ HawkcError hawkc_parse_auth_header(HawkcContext ctx, char *value, size_t len, Ha
 			remain -= n;
 		}
 
+		/* There can be optional WS after key/value pair */
 		consume_ows(ctx,p,remain,&n);
 		p += n;
 		remain -= n;
 
+		/* Now pass key and value to callback */
 		if( (e = param_handler(ctx,key,value,data)) != HAWKC_OK) {
-					return e;
-			}
+				return e;
+		}
 
-
+		/* If we have more, consume delimiter and optional WS */
 		if(remain > 0) {
 			if(*p == ',') {
 				p++;
@@ -177,21 +193,13 @@ HawkcError hawkc_parse_auth_header(HawkcContext ctx, char *value, size_t len, Ha
 				p += n;
 				remain -= n;
 			} else {
-				return hawkc_set_error(ctx,
-								HAWKC_PARSE_ERROR, "',' required after parameter value");
+				/* Delimiter must be , */
+				return hawkc_set_error(ctx, HAWKC_PARSE_ERROR, "',' required after parameter value");
 			}
 		}
 	}
 	return HAWKC_OK;
 }
-#define IS_TOKEN(c) ( \
-	   ( (c) >= '0' && (c) <= '9') \
-	|| ( (c) >= 'A' && (c) <= 'Z') \
-	|| ( (c) >= '^' && (c) <= 'z') \
-	|| ( (c) >= '#' && (c) <= '\'') \
-	|| ( (c) == '!') || ((c) == '*') || ((c) == '+') || ((c) == '-') || ((c) == '.') )
-
-#define IS_SPACE(c) ( ((c) == ' ') || ((c) == '\t') )
 
 /**
   * Parses a token as defined by
@@ -208,36 +216,51 @@ HawkcError parse_token(HawkcContext ctx, char *s, size_t len, HawkcString *ptoke
 		p++;
 	}
 	if(i == 0) {
-		return hawkc_set_error(ctx,
-						HAWKC_PARSE_ERROR, "Token must have at least one character");
+		return hawkc_set_error(ctx, HAWKC_PARSE_ERROR, "Token must have at least one character");
 	}
 	*n = i;
 	ptoken->len = i;
 	return HAWKC_OK;
 }
 
+/*
+ * Parse quoted string from string s with length len and store
+ * number of bytes read in n; Store the string read in
+ * ptoken.
+ *
+ * Escape characters will not be stripped. For example, if
+ * the parsed string s contains "he said: \"Wow!\"" ptoken will be set
+ * to contain he said: \"Wow!\".
+ *
+ * Removing the quotes would make it necessary to make a copy
+ * of the parsed string and we want to avoid that.
+ */
 HawkcError parse_quoted_text(HawkcContext ctx, char *s, size_t len, HawkcString *ptoken, size_t *n) {
 	char *p = s;
 	size_t i = 0;
 
-
 	if(len == 0 || *p != '"') {
-		return hawkc_set_error(ctx,
-						HAWKC_PARSE_ERROR, "Quoted text must start with '\"'");
+		return hawkc_set_error(ctx, HAWKC_PARSE_ERROR, "Quoted text must start with '\"'");
 	}
-	/* consume " */
+	/* consume starting " */
 	p++;
 	i++;
-
 
 	ptoken->data = p;
 	ptoken->len = 0;
 
+	/*
+	 * Consume tokens as long as we have some
+	 */
 	while(i < len && *p != '"') {
+		/*
+		 * Consume escaped token, make sure there is
+		 * a token following the \ which we will blindly
+		 * just consume.
+		 */
 		if(*p == '\\') {
 			if(i+1 == len) {
-				return hawkc_set_error(ctx,
-						HAWKC_PARSE_ERROR, "\\ at end of text");
+				return hawkc_set_error(ctx, HAWKC_PARSE_ERROR, "\\ at end of text");
 			}
 			p++;
 			i++;
@@ -247,19 +270,24 @@ HawkcError parse_quoted_text(HawkcContext ctx, char *s, size_t len, HawkcString 
 		i++;
 		ptoken->len++;
 	}
+	/*
+	 * There must be a token left (which will be ", given the while condition above).
+	 */
 	if(i >= len) {
-		return hawkc_set_error(ctx,
-						HAWKC_PARSE_ERROR, "Quoted text must end with '\"'");
+		return hawkc_set_error(ctx, HAWKC_PARSE_ERROR, "Quoted text must end with '\"'");
 	}
 
-	/* consume " */
+	/* consume ending " */
 	p++;
 	i++;
 
 	*n = i;
 	return HAWKC_OK;
 }
-
+/*
+ * Parse optional whitespace from string s with length len and store
+ * number of bytes read in n;
+ */
 void consume_ows(HawkcContext ctx, char *s, size_t len, size_t *n) {
 	size_t i = 0;
 	char *p = s;
@@ -270,5 +298,8 @@ void consume_ows(HawkcContext ctx, char *s, size_t len, size_t *n) {
 	}
 	*n = i;
 }
+
+
+
 
 
